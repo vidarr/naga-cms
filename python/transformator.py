1449d053dee85857c3b6fba1d4205427c8881154
Michael J. Beer <michael.josef.beer@googlemail.com>
2014-01-29 22:08:40 +0000

    MIB: UPDATE - Added some standard handlers to produce html out of mark up text.

--- python/transformator.py
+++ python/transformator.py
@@ -14,15 +14,75 @@ import nagaUtils
 #------------------------------------------------------------------------------
 _logger = logging.getLogger('transformator')
 #------------------------------------------------------------------------------
+# Standard call backs to convert to html
+#------------------------------------------------------------------------------
+def heading(call, arg):
+    return '<h>' + arg + '</h>'
+#------------------------------------------------------------------------------
+def listing(call, arg):
+    sur_tag = 'ol'
+    if call == 'ulist':
+        sur_tag = 'ul'
+    elements = arg.split('*')
+    if len(elements) < 2:
+        _logger.warn("wont build empty list from " + arg)
+        return ''
+    result = ['<', sur_tag, '>']
+    for item in elements[1:]:
+        result.append('<li>')
+        result.append(item)
+        result.append('</li>')
+    result.append('<')
+    result.extend(['</', sur_tag, '>'])
+    return ''.join(result)
+#------------------------------------------------------------------------------
+def bold(call, arg):
+    return '<b>' + arg + '</b>'
+#------------------------------------------------------------------------------
+def italic(call, arg):
+    return '<i>' + arg + '</i>'
+#------------------------------------------------------------------------------
+def line_break(call, arg):
+    return '<br>'
+#------------------------------------------------------------------------------
+def make_link(separator):
+    def link(call, arg):
+        parts = arg.partition(separator)
+        (target, description) = (parts[0], parts[2])
+        if call == 'ilink':
+            # ilink will link to internal article
+            print ("internal link has not been implemented yet")
+            return arg
+        if description == '':
+            description = target
+        return '<a href="' + target + '">' + description + '</a>'
+    return link
+#------------------------------------------------------------------------------
 class Transformator:
     #---------------------------------------------------------------------------
-    def __init__(self):
+    def __init__(self, **params):
         self.left_marker = '['
-        self.quoted_left_marker = self.left_marker + self.left_marker
+        if 'left_marker' in params:
+            self.left_marker = params['left_marker']
         self.right_marker = ']'
-        self.quoted_right_marker = self.right_marker + self.right_marker
+        if 'right_marker' in params:
+            self.right_marker = params['right_marker']
         self.separator = ' '
-        self.lookup = {}
+        if 'separator' in params:
+            self.separator = params['separator']
+        self.lookup = {
+                'heading' : heading, 
+                'olist'   : listing,
+                'ulist'   : listing,
+                'br'      : line_break,
+                'b'       : bold,
+                'i'       : italic,
+                'link'    : make_link(self.separator)
+                }
+        if 'handlers' in params:
+            self.lookup = params['handlers']
+        self.quoted_left_marker = self.left_marker + self.left_marker
+        self.quoted_right_marker = self.right_marker + self.right_marker
         self._logger = logging.getLogger('Transformator')
     #---------------------------------------------------------------------------
     def register_callback(self, identifier, callback):
@@ -49,7 +109,11 @@ class Transformator:
                 inner_part = origin_string[:-1]
             else:
                 if origin_string[position + 1] == self.right_marker:
-                    position = position + 1
+                    if len(origin_string) < position + 2 + 1:
+                        self._logger.error(origin_string + 
+                        " does not contain end marker")
+                        return None
+                    position = position + 2
                 else:
                     # We found the end - now cut it out
                     inner_part = origin_string[:position]
@@ -58,13 +122,13 @@ class Transformator:
                 self.left_marker).replace(self.quoted_right_marker,
                         self.right_marker)
         self._logger.info("got format call " + inner_part)
-        print("got format call " + inner_part)
         call_parts = inner_part.partition(self.separator)
         if call_parts[0] == '':
             self._logger.error("error when trying to split " + inner_part)
             return None
         if call_parts[0] in self.lookup:
-            transformed_parts.append(self.lookup[call_parts[0]](inner_part))
+            transformed_parts.append(self.lookup[call_parts[0]](call_parts[0],
+                call_parts[2]))
         else:
             transformed_parts.append(inner_part)
         return rest
@@ -88,119 +152,4 @@ class Transformator:
                 # BINGO - we got a format call
                 origin_string = self.treat_format_call(parts[2], 
                         transformed_string)
-
-#         parts = origin_string.partition(self.left_marker)
-#         while parts[1] != '':
-#             if parts[0].startswith(self.left_marker):
-#                 # Non-caught case: String started with '[' 
-#                 # we encountered a quoted marker...
-#                 transformed_string.append(self.left_marker)
-#                 # if parts[2] == '' the original string ended in '[[[' - no
-#                 # sense whatsoever ...
-#                 if parts[2] == '':
-#                     self._logger.error("String seems to end in [[[ : " +
-#                             origin_string)
-#                     return None
-#                 stripped_string = parts[2]
-#                 stripped_string = stripped_string[1:]
-#                 converted_string_parts = treat_call(stripped_string)
-#                 if converted_string_parts == None:
-#                     self._logger.error("right marker missing " + origin_string)
-#                     return None
-        # TODO: Implement further
-    #---------------------------------------------------------------------------
-    def treat_call(self, stripped_string):
-        parts = stripped_string.partition(self.right_marker)
-        if parts[1] == '':
-            return None
-        call_parts = parts[0].partition(self.separator)
-        handler = call_parts[0]
-        if handler in self.lookup:
-            parts[0] = self.lookup[handler](call_parts[2])
-        return [parts[0], parts[2]]
-
-
-
-
-
-
-
-
-
-        # string_parts = origin_string.split(self.left_marker)
-        # string_parts.reverse()
-        # if len(string_parts) < 1:
-        #    self._logger.error("Parse error on " + origin_string + " at " + string_parts)
-        #    print("Parse error on " + origin_string + " at " + string_parts)
-        #    return None
-        # transformed_string.append(string_parts.pop())
-        # while len(string_parts) > 0:
-        #     next_part = string_parts.pop()
-        #     if next_part == '':
-        #         transformed_string.append(self.left_marker)
-        #     else:
-        #         inner_parts = []
-        #         found_end = False
-        #         parts = next_part.split(self.right_marker)
-        #         parts.reverse()
-        #         # we ignore the case of an '[[]'
-        #         if len(parts) < 1:
-        #             self._logger.error("Parse error on " + origin_string + " at " + parts)
-        #             print("Parse error on " + origin_string + " at " + parts)
-        #             return None
-        #         inner_parts.append(parts.pop())
-        #         while not found_end and len(parts) > 0:
-        #             part = parts.pop()
-        #             if part == '': 
-        #                 inner_parts.append(self.right_marker)
-        #             else:
-        #                 inner_parts.append(part)
-        #                 found_end = True
-        #         if not found_end:
-        #             self._logger.error("Parse error on " + origin_string + " found opening marker before closing marker")
-        #             print("Parse error on " + origin_string + " found opening marker before closing marker")
-        #             return None
-        #         # Discover handler and hand over to handler
-        #         handler = inner_parts[0]
-        #         result = ''.join(inner_parts)
-        #         if handler in self.lookup:
-        #             result = self.lookup[handler](result)
-        #         else:
-        #             self._logger.warn("No handler for " + handler + " found")
-        #         transformed_string.append(result)
-        #         # '' have been quoted ']', we do not check for single '[' -
-        #         # they should not appear at this point ...
-        #         rest = [self.right_marker if x == ''  else x for x in parts]
-        #         transformed_string.append(''.join(rest))
-        # return ''.join(transformed_string)
-        # while len(origin_string) > 0:
-        #     tokens_before = self.left_separator_pattern.split(origin_string, 1)
-        #     if len(tokens_before) < 2:
-        #         transformed_string.append(origin_string)
-        #         return self.collapse_string(transformed_string)
-        #     transformed_string.append(self.strip_escapes(tokens_before[0]))
-        #     tokens_after = self.right_separator_pattern.split(tokens_before[1], 1)
-        #     if len(tokens_after) < 2:
-        #         self._logger.error("No closing separator found in " +
-        #                 tokens_before[1])
-        #         return self.collapse_string(transformed_string)
-        #     origin_string = tokens_after[1]
-        #     transform_tokens = tokens_after[0].split(self.separator)
-        #     if len(transform_tokens) < 1 or not transform_tokens[0] in self.lookup:
-        #         transformed_string.append(self.separator.join(transform_tokens))
-        #     else:
-        #         handler = self.lookup[transform_tokens[0]]
-        #         transformed_string.append(handler(transform_tokens))
-        # return self.collapse_string(transformed_string)
-    #---------------------------------------------------------------------------
-    def strip_escapes(self, esc_string):
-        return esc_string.replace(self.left_marker + self.left_marker,
-                self.left_marker).replace(self.right_marker + self.right_marker,
-                        self.right_marker)
-    #---------------------------------------------------------------------------
-    def collapse_string(self, result_string_parts):
-        print("Got " + ' '.join(result_string_parts))
-        return ''.join([self.strip_escapes(token) for token in 
-            result_string_parts])
-
 
--- test/test_transformator.py
+++ test/test_transformator.py
@@ -19,8 +19,8 @@ _logger       = logging.getLogger("test_registry")
 #-------------------------------------------------------------------------------
 # Helpers
 #-------------------------------------------------------------------------------
-def handler1(tokens):
-    return "h1:" + tokens
+def handler1(call, tokens):
+    return "h1:" + call + ' ' + tokens
 #==============================================================================
 class textTransformator(unittest.TestCase):
     '''Test transformator'''
@@ -53,16 +53,42 @@ class textTransformator(unittest.TestCase):
         res_string = "abcdefgh1:heading ijklmnopqrstuvwxyz1234567890"
         self.assertEqual(res_string, self.transformator.transform(test_string))
     #--------------------------------------------------------------------------
-    # def test_link(self):
-    #     pass
-        # return test_transform('''Hagbard [link
-        # https://de.wikipedia.org/wiki/Hagbard Germanische Sagengestalt]
-        # Celine''', '''Hagbard <a
-        # href="https://de.wikipedia.org/wiki/Hagbard">Germanische
-        # Sagengestalt</a>''')
+    def test_transform_beginning(self):
+        test_string = "[heading ijk]lmnopqrstuvwxyz1234567890"
+        res_string = "h1:heading ijklmnopqrstuvwxyz1234567890"
+        self.assertEqual(res_string, self.transformator.transform(test_string))
+    #--------------------------------------------------------------------------
+    def test_transform_end(self):
+        test_string = "abcdefg[heading ijk]"
+        res_string = "abcdefgh1:heading ijk"
+        self.assertEqual(res_string, self.transformator.transform(test_string))
+    #--------------------------------------------------------------------------
+    def test_transform_escapes(self):
+        test_string = "[[ab]]cdefg[heading ijk]12345]]]]67890[["
+        res_string = "[ab]cdefgh1:heading ijk12345]]67890["
+        self.assertEqual(res_string, self.transformator.transform(test_string))
+    #--------------------------------------------------------------------------
+    def test_transform_nested_escapes(self):
+        test_string = "[[ab]]cdefg[heading i[[j]]k]12345]]]]67890"
+        res_string = "[ab]cdefgh1:heading i[j]k12345]]67890"
+        self.assertEqual(res_string, self.transformator.transform(test_string))
+    #--------------------------------------------------------------------------
+    def test_unknown_call(self):
+        test_string = "[[ab]]cdefg[footer i[[j]]k]12345]]]]67890"
+        res_string = "[ab]cdefgfooter i[j]k12345]]67890"
+        self.assertEqual(res_string, self.transformator.transform(test_string))
 #------------------------------------------------------------------------------
 # MAIN
 #------------------------------------------------------------------------------
 if __name__ == '__main__':
     logging.basicConfig(filename='test_transformator.log',level=logging.DEBUG)
     unittest.main()
+#     trans = transformator.Transformator()
+#     print (trans.transform('''[heading Once upon a time] 
+#  There has been some sort of list, it was 
+#  [olist * one *two *three]
+#  or 
+#  [ulist *an item *and another]
+#  No one knew what it was about
+#  Others [link https://google.com linked] to the good old [link http://google.com]'''))
+# 
